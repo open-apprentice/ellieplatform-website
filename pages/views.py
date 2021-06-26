c5b38444b387451f95b6f686b489e177116efc4e
davidkartuzinski <david@kaidawei.me>
2021-06-26 15:49:08 +0200

    urls for home page now working

--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,15 @@ from django.shortcuts import render, get_object_or_404
 from .models import Page
 
 
+def index(request):
+    page = get_object_or_404(Page, slug='home', status='published')
+
+    context = {
+        'page': page
+    }
+    return render(request, 'pages/index.html', context)
+
+
 def single_page(request, slug):
     page = get_object_or_404(Page, slug=slug, status='published')
 
@@ -10,4 +19,3 @@ def single_page(request, slug):
     }
 
     return render(request, 'pages/single_page.html', context)
-
